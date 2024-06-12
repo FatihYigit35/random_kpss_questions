48e61db1ca7076fae886d16c8bf0cc7398bdf540
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-06-13 00:48:02 +0300

    On the answers page, questions from the list appear dynamically and when clicked, it moves to the next question.

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:random_kpss_questions/app_colors.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton(this.text, {super.key});
+  const AnswerButton({super.key, required this.text, required this.onClick});
 
   final String text;
+  final void Function() onClick;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
-      child: Text(text),
+      onPressed: onClick,
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: AppColors.primaryColor,
+          foregroundColor: Colors.white),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
+      ),
     );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:random_kpss_questions/answer_button.dart';
 import 'package:random_kpss_questions/app_colors.dart';
+import 'package:random_kpss_questions/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,33 +12,50 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void onClickAnswer() {
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: AppColors.backgroundColor,
         body: SizedBox(
           width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Questions Screen"),
-              SizedBox(
-                height: 30,
-              ),
-              AnswerButton(
-                'Cevap1',
-              ),
-              AnswerButton(
-                'Cevap2',
-              ),
-              AnswerButton(
-                'Cevap3',
-              ),
-              AnswerButton(
-                'Cevap4',
-              ),
-            ],
+          child: Container(
+            margin: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  currentQuestion.text,
+                  style: GoogleFonts.openSans(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ...currentQuestion.answers.map(
+                  (answer) => AnswerButton(
+                    text: answer,
+                    onClick: onClickAnswer,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
