7a62a84dd0f00ef15c63cc9611fb7d9c05791386
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-06-18 16:43:37 +0300

    Unused codes were deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:random_kpss_questions/theme/app_colors.dart';
 import 'package:random_kpss_questions/view/quiz.dart';
 
 void main() {
   runApp(const Quiz());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        body: const Quiz(),
-      ),
-    );
-  }
 }
